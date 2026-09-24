6e712a2c8e77d359ab7c2e511db2e5e8cb46d241
Nabou <nabou.diengstrife@slalom.com>
2026-09-24 15:56:20 -0400

    new verification

---
+++ verify-objects.sql
@@ -0,0 +1,8 @@
+-- Verify warehouses and databases in the account.
+SHOW WAREHOUSES;
+SHOW DATABASES;
+
+-- Verify schemas, tables, and views in each accessible database.
+SHOW SCHEMAS IN ACCOUNT;
+SHOW TABLES IN ACCOUNT;
+SHOW VIEWS IN ACCOUNT;
